e7e84f32200aeb1dd9942f6c2cb83a442b3f507e
Eko Misprajiko <ekomisprajiko@gmail.com>
2020-07-13 16:45:24 +0700

    Resposive Design (Video3)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div>
-    <img class="inline-flex" alt="Vue logo" src="../assets/img/logo.png" />
-    <HelloWorld msg="Hello World!" />
+  <div class="flex bg-white">
+    <div
+      class="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:max-w-full lg:w-1/2 lg:py-24 lg:px-12"
+    >
+      <div class="w-full mx-auto">
+        <a
+          href="#"
+          class="text-2xl leading-none font-dosis font-medium sm:text-center"
+        >
+          <img
+            class="inline-block align-middle h-10 mr-2 sm:h-15 sm:mr-4 md:h-15 md:mr-3"
+            alt="Vue School"
+            src="../assets/img/logo.png"
+          />
+          <span class="sm:hidden md:inline-block md:text-4xl md:align-middle"
+            >Vue.js</span
+          >
+        </a>
+      </div>
+      <img
+        class="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden"
+        alt="Vue Logo"
+        src="../assets/img/vue-logo.png"
+      />
+      <h1
+        class="mt-6 text-2xl font-bold text-blue-900 leading-tight sm:mt-8 sm:text-4xl"
+      >
+        Vue (pronounced /vjuː/, like view) is:
+        <span class="text-green-700"
+          >a progressive framework for building user interfaces.</span
+        >
+      </h1>
+      <p class=" mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+        Unlike other monolithic frameworks, Vue is designed from the ground up
+        to be incrementally adoptable. The core library is focused on the view
+        layer only, and is easy to pick up and integrate with other libraries or
+        existing projects.
+      </p>
+      <div class="mt-4 sm:mt-6">
+        <a
+          href="#"
+          class=" inline-block px-5 py-3 rounded-lg shadow-lg bg-green-600 text-sm text-white uppercase tracking-wider font-semibold sm:text-base"
+          >Get Started Here!</a
+        >
+      </div>
+    </div>
+    <div class="hidden lg:block lg:w-1/2 lg:relative">
+      <img
+        class="absolute inset-0 mx-auto m-auto"
+        alt="Vue Logo"
+        src="../assets/img/vue-logo.png"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
