102cc4e4044e45db789731d043acca19be8a1844
Eko Misprajiko <ekomisprajiko@gmail.com>
2020-07-13 10:38:05 +0700

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="block w-full items-center">
+    <img class="inline-flex" alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
