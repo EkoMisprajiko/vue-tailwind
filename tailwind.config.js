279e002d2d4bc32eb5c56a05a229e64052870638
Eko Misprajiko <ekomisprajiko@gmail.com>
2020-07-13 16:43:08 +0700

    add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: [
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji"
+        ],
+        serif: ["Georgia", "Cambria"],
+        mono: ["SFMono-Regular", "Menlo"],
+        dosis: [
+          '"Dosis"',
+          '"Source Sans Pro"',
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif"
+        ]
+      }
+    }
   },
   variants: {},
   plugins: []
