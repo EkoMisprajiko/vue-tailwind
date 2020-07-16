661091577ae0847e8d7c11c3d952b88ebc99ef66
Eko Misprajiko <ekomisprajiko@gmail.com>
2020-07-16 16:00:18 +0700

    Just a little modification

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,39 +2,23 @@
   <div>
     <div class="flex bg-white">
       <div
-        class="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:max-w-full lg:w-1/2 lg:py-24 lg:px-12"
+        class="px-8 py-12 max-w-xl mx-auto sm:max-w-xl lg:max-w-full lg:w-1/2 lg:w-2/3 lg:py-12 lg:px-12 xl:pr-0"
       >
-        <div class="xl:max-w-lg xl:ml-auto">
-          <div class="w-full mx-auto">
-            <a
-              href="#"
-              class="text-2xl leading-none font-dosis font-medium sm:text-center"
-            >
-              <img
-                class="inline-block align-middle h-10 mr-2 sm:h-15 sm:mr-4 md:h-15 md:mr-3"
-                alt="Vue Small Logo"
-                src="../assets/img/logo.png"
-              />
-              <span
-                class="sm:hidden md:inline-block md:text-4xl md:align-middle"
-                >Vue.js</span
-              >
-            </a>
-          </div>
+        <div class="xl:max-w-full xl:ml-auto">
           <img
-            class="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-88 sm:w-full md:h-100 lg:hidden"
+            class="mt-0 h-88 w-full mx-auto max-w-md lg:hidden"
             alt="Vue Big Logo"
             src="../assets/img/vue-logo.png"
           />
           <h1
-            class="mt-6 text-2xl font-bold text-blue-900 leading-tight sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl"
+            class="mt-8 text-2xl font-bold text-blue-900 leading-tight sm:mt-8 sm:text-4xl lg:mt-0 lg:text-3xl xl:text-4xl"
           >
             Vue (pronounced like view) is:
             <br class="hidden lg:inline" /><span class="text-green-700"
               >a progressive framework for building user interfaces.</span
             >
           </h1>
-          <p class=" mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+          <p class="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
             Unlike other monolithic frameworks, Vue is designed from the ground
             up to be incrementally adoptable. The core library is focused on the
             view layer only, and is easy to pick up and integrate with other
@@ -49,19 +33,21 @@
           </div>
         </div>
       </div>
-      <div class="hidden lg:block lg:w-1/2 lg:relative">
-        <img
-          class="absolute inset-0 m-auto mr-8 xl:inset-0 xl:m-auto xl:mr-20"
-          alt="Vue Logo"
-          src="../assets/img/vue-logo.png"
-        />
+      <div class="hidden py-1/50 lg:block lg:w-1/2 lg:pr-6 xl:w-1/3">
+        <div class="relative h-full lg:max-w-md xl:mx-auto">
+          <img
+            class="absolute h-full w-full"
+            alt="Vue Logo"
+            src="../assets/img/vue-logo.png"
+          />
+        </div>
       </div>
     </div>
 
-    <div class="p-5 m-auto sm:p-4 md:p-4 lg:p-4 xl:p-4">
-      <div class="flex flex-wrap">
+    <div class="px-3 py-2 m-auto sm:px-3 md:px-3 lg:px-4 xl:px-4">
+      <div class="flex flex-wrap m-auto">
         <div
-          class="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3"
+          class="flex flex-col px-3 pt-4 pb-2 w-full sm:w-1/2 lg:w-1/3 xl:w-1/4"
           v-for="property in properties"
           :key="property"
         >
